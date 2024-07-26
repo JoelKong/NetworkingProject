44ee030a25eb136ad8a2bf2151d16361bae76794
Edric <edric.chan.1997@gmail.com>
2024-07-26 13:08:52 +0800

    chore: remove website files

--- website/src/lib/api/modify.ts
+++
@@ -1,5 +0,0 @@
-export const activateSensors = () => {
-  return fetch('/api/activate', {
-    method: 'PUT'
-  });
-};
--- website/src/lib/ui/settings/settings-dialog-types.ts
+++
@@ -1,8 +0,0 @@
-import type { Settings } from '$lib/data/settings';
-
-export interface SettingsDialogContentProps {
-  klass: string;
-  settings: Settings;
-
-  onSettingsChange: (settings: Settings) => void;
-}
